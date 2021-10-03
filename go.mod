a2ada35604090e3188f438e604f1709fbc0e1b84
Andrew Snodgrass <git@pointlander.info>
2021-10-02 19:38:00 -0500

    Neural network

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pointlander/rand
 
 go 1.16
 
-require gonum.org/v1/plot v0.10.0 // indirect
+require (
+	github.com/pointlander/gradient v0.0.0-20201206051041-dbff480e6d28 // indirect
+	gonum.org/v1/plot v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,11 +5,16 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+
+	"github.com/pointlander/gradient/sf64"
 )
 
 // Size is the size of the network
@@ -46,6 +51,98 @@ func main() {
 	}
 	p.Add(histogram)
 	err = p.Save(8*vg.Inch, 8*vg.Inch, "histogram.png")
+	if err != nil {
+		panic(err)
+	}
+
+	var weights = [Size]sf64.V{}
+	for i := range weights {
+		weights[i].X = 7 * rand.ExpFloat64()
+	}
+	var state = [Size]sf64.Meta{}
+	for i := range state {
+		state[i] = weights[i].Meta()
+	}
+	var outputs = [3]sf64.V{}
+	for i := range outputs {
+		outputs[i].X = 3 * rand.ExpFloat64()
+	}
+	var sum = [3]sf64.Meta{}
+	for j := range sum {
+		for i := range state {
+			state[i] = sf64.Sin(sf64.Mul(weights[0].Meta(), state[i]))
+			if sum[j] == nil {
+				sum[j] = state[i]
+			} else {
+				sum[j] = sf64.Add(sum[j], state[i])
+			}
+		}
+	}
+	cost := sf64.Sub(outputs[0].Meta(), sum[0])
+	cost = sf64.Mul(cost, cost)
+	for i := 1; i < len(outputs); i++ {
+		diff := sf64.Sub(outputs[i].Meta(), sum[i])
+		diff = sf64.Mul(diff, diff)
+		cost = sf64.Add(cost, diff)
+	}
+
+	eta, iterations := .001, 1024
+	points := make(plotter.XYs, 0, iterations)
+	i := 0
+	for i < iterations {
+		total := 0.0
+		for i := range weights {
+			weights[i].D = 0
+		}
+
+		total += sf64.Gradient(cost).X
+		s := 0.0
+		for i := range weights {
+			d := weights[i].D
+			s += d * d
+		}
+		norm := math.Sqrt(s)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+
+		for i := range weights {
+			weights[i].X -= eta * weights[i].D * scaling
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: total})
+
+		fmt.Println(i, total)
+		i++
+		if total < 1e-4 {
+			for j := range outputs {
+				var y float64
+				sum[j](func(a *sf64.V) bool {
+					y = a.X
+					return true
+				})
+				fmt.Println(outputs[j].X, y)
+			}
+			break
+		}
+	}
+
+	p = plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "cost.png")
 	if err != nil {
 		panic(err)
 	}
